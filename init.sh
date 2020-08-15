f6de6e73081f5e7de2e5a6832ad5e57c8d2229d6
Pavel Dubov <PADking888@yandex.ru>
2020-08-15 19:43:48 +0300

    Пробное решение п.3 задачи 1.9.11 (попытка 9)

--- init.sh
+++ init.sh
@@ -1,16 +1,15 @@
-# sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-# sudo /etc/init.d/gunicorn restart
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.conf.py
+sudo /etc/init.d/gunicorn restart
+
 
 # Проверка правильности выполнения п.3 задания
 # gunicorn -c /etc/gunicorn.d/hello hello:app
 # gunicorn -w 2 -b 0.0.0.0:8080 hello:app
-# curl http://localhost:8080/?a=1&a=2&b=3
-# curl http://127.0.0.1:8080/?a=1&a=2&b=3
-# curl -vv http://127.0.0.1/hello/?a=1&a=2&b=3
-# curl -vv 127.0.0.1:8080/?a=1&a=2&b=3
+# curl 'http://localhost:8080/?a=1&a=2&b=3'
+# curl 'http://127.0.0.1:8080/?a=1&a=2&b=3'
+# curl -vv 'http://127.0.0.1/hello/?a=1&a=2&b=3'
+# curl -vv '127.0.0.1:8080/?a=1&a=2&b=3'
+
 # Завершена с ошибкой:
 # Starting Gunicorn workers                                                    
 # Traceback (most recent call last):                                              
@@ -20,3 +19,8 @@ sudo /etc/init.d/nginx restart
 #     module = imp.load_source(filename, filename)                                
 # IOError: [Errno 2] No such file or directory
 
+
+
+
+# git clone https://github.com/Padking/web.git
+# gunicorn -w 2 -b 0.0.0.0:8080 hello:app & curl -vv 'http://127.0.0.1/hello/?a=1&a=2&b=3'
