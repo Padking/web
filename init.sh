cbf706f1a33b5656b3f04e02a724772cfd4a4572
Pavel Dubov <PADking888@yandex.ru>
2020-08-15 19:30:55 +0300

    Пробное решение задачи 1.9.11 (попытка 2)

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+# sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+# sudo /etc/init.d/gunicorn restart
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
+sudo /etc/init.d/nginx restart
 
 # Проверка правильности выполнения п.3 задания
 # gunicorn -c /etc/gunicorn.d/hello hello:app
 # gunicorn -w 2 -b 0.0.0.0:8080 hello:app
 # curl http://localhost:8080/?a=1&a=2&b=3
-curl -vv 127.0.0.1:8080/?a=1&a=2&b=3
+# curl http://127.0.0.1:8080/?a=1&a=2&b=3
+# curl -vv http://127.0.0.1/hello/?a=1&a=2&b=3
+# curl -vv 127.0.0.1:8080/?a=1&a=2&b=3
 # Завершена с ошибкой:
 # Starting Gunicorn workers                                                    
 # Traceback (most recent call last):                                              
