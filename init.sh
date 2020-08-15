550f9eda212fb86b4c6b9f70fa5a467cc8f9a91b
Pavel Dubov <PADking888@yandex.ru>
2020-08-15 14:24:48 +0300

    Пробное решение п.3 задачи 1.9.11 (попытка 6)

--- init.sh
+++ init.sh
@@ -5,12 +5,11 @@ sudo /etc/init.d/gunicorn restart
 gunicorn -c /etc/gunicorn.d/hello hello:app
 curl http://localhost:8080/?a=1&a=2&b=3
 # Завершена с ошибкой:
-"""
-Starting Gunicorn workers                                                    
-Traceback (most recent call last):                                              
-  File "/usr/sbin/gunicorn-debian", line 218, in <module>                       
-    sys.exit(main())                                                            
-  File "/usr/sbin/gunicorn-debian", line 84, in main                            
-    module = imp.load_source(filename, filename)                                
-IOError: [Errno 2] No such file or directory
-"""
+# Starting Gunicorn workers                                                    
+# Traceback (most recent call last):                                              
+#   File "/usr/sbin/gunicorn-debian", line 218, in <module>                       
+#     sys.exit(main())                                                            
+#   File "/usr/sbin/gunicorn-debian", line 84, in main                            
+#     module = imp.load_source(filename, filename)                                
+# IOError: [Errno 2] No such file or directory
+
