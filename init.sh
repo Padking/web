d012e05ca4d7f0e23eedb6554d42ce82a24cc0f4
Pavel Dubov <PADking888@yandex.ru>
2020-08-15 12:53:37 +0300

    Начальный коммит

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_8_12.conf
+sudo /etc/init.d/nginx restart
+
+# Проверка правильности выполнения задания
+curl http://localhost/uploads/proba.txt  # выполнить в оболочке терминала степик
