6001bb3240fa7cbd3951be3912389269497d92bf
Pavel Dubov <PADking888@yandex.ru>
2020-08-16 17:19:02 +0300

    Пробное решение задачи 1.9.11 (попытка 2)

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/gunicorn.d/django.example
+sudo rm -rf /etc/gunicorn.d/wsgi.example
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_1_9_11.conf
+sudo ln -s /home/box/web/etc/hello.conf /etc/gunicorn.d/hello
+
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
