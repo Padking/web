b59a54398a800bd410d421c53aa83a102cb6ddf5
Pavel Dubov <PADking888@yandex.ru>
2020-08-16 21:23:13 +0300

    Пробное решение задачи 2.1.11 (попытка 2)

--- init.sh
+++ init.sh
@@ -9,3 +9,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
 
 sudo /etc/init.d/nginx restart
+
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
