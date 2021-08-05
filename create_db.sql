68f19b671f49bddd3dc9ba8822303b397b27a211
Pavel Dubov <PADking888@yandex.ru>
2021-08-05 11:28:24 +0300

    Обеспечение работы с БД

---
+++ create_db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE ask_app_db CHARACTER SET utf8;
--- init.sh
+++ init.sh
@@ -9,5 +9,8 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
 
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
 
 gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
+
+mysql -u root -e "CREATE DATABASE ask_app_db CHARACTER SET utf8;"
